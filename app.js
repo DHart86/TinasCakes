2d0ee868fe205bb5956d6096db73644085eb258b
Dave Hartley <djhartley86@yahoo.com>
2017-01-15 18:59:19 -0800

    click touch

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ $('.cakePic').on("click", function() {
   $('#modalPic').attr("src", thisSrc);
 })
 
-$(".profImg").on("dblclick touchstart", function() {
+$(".profImg").on("dblclick click", function() {
   $("#TMC").text("JK IT'S ALL SEXUAL FAVORS");
   $("html, body").animate({ scrollTop: 0 }, "slow");
 })  
