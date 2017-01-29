befef416f9b0cbd10e493a6886aec2f1e75021c6
DHart86 <djhartley86@yahoo.com>
2017-01-29 13:59:01 -0500

    added stock tina

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
       zoom: 12,
       scrollwheel: false,
       draggable: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ffa8a8"},{"visibility":"on"}]}]
     };
 
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
@@ -55,27 +56,10 @@ $('.cakePic').on("click", function() {
   $('#modalPic').attr("src", thisSrc);
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".profImg").on("dblclick", function() {
+  $("#TMC").text("JK IT'S ALL SEXUAL FAVORS");
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+})  
+  
+  
 })
